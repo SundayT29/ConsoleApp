143200f021a35ac3790b67584ec9b44bd167a2af
Timothy Adebisi <tadebisi@ahnigeria.org>
2023-08-24 15:43:15 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Tutorial
+{
+    class ConsoleApp
+    {
+        static void Main(string[] args)
+        {
+            /*
+             How to declare Variables
+
+            string name;
+            int age;
+
+            name = "John";
+            age = 20;
+
+            Console.WriteLine("There was once a man named " + name);
+            Console.WriteLine("He was " + age + " years old.");
+
+            */
+
+            //Data Types
+            string Name = "James is my name";
+            Char Grade = 'A';
+            int Age = 10;
+            double Value = 12.0233; // You can use float least accurate or decimal which is most accurate.
+            bool isMale = false;
+
+            // Arrays
+
+            //int[] Numbers = Array.Empty<int>();
+
+            // If Statement
+
+            if (true)
+            {
+
+            }
+        }
+    }
+}
